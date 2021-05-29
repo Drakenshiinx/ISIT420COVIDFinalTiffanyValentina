51c2f2ca1defcc6d4adceb414df66f2741fdf715
Tiffany Cole <Drakenshiinx@hotmail.com>
2021-05-29 16:29:39 -0700

    added query to query 1

--- ISIT420COVIDFinalTiffanyValentinaSolution/ISIT420COVIDFinalTiffanyValentina/Controllers/Query1Controller.cs
+++ ISIT420COVIDFinalTiffanyValentinaSolution/ISIT420COVIDFinalTiffanyValentina/Controllers/Query1Controller.cs
@@ -9,5 +9,30 @@ namespace ISIT420COVIDFinalTiffanyValentina.Controllers
 {
     public class Query1Controller : ApiController
     {
+        CovidDeathsDBEntities myDB = new CovidDeathsDBEntities();
+
+        [Route("api/covid/query1")]
+        [HttpGet]
+        public IHttpActionResult GetPositiveTests()
+        {
+            DateTime dateSpecific = new DateTime(2020, 06, 19);
+
+            var CovidTestPos = from NursingHome in myDB.NursingHomeDeathsTables
+                               from Towns in myDB.TownCTDeathsTables
+                               where Towns.Date == dateSpecific && NursingHome.Date == dateSpecific
+                               select new 
+                               {
+                                   Towns.Date,
+                                   NursingHome.NursingHome,
+                                   Towns.TownName,
+                                   NursingHome.COVIDDeath,
+                                   Towns.ConfirmedCOVIDDeaths,
+                                   NursingHome.COVIDPositive,
+                                   Towns.TestPositive
+                               };
+
+            return Json(CovidTestPos);
+        }
+
     }
 }
