c20e2c05403d84c78296781cd59177f56c0a9b9b
ValentinkaM <va_m_ru@yahoo.com>
2021-05-30 00:44:18 -0700

    Query 2 and Query 3 are working

---
+++ ISIT420COVIDFinalTiffanyValentinaSolution/ISIT420COVIDFinalTiffanyValentina/Controllers/Query2and3Controller.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ISIT420COVIDFinalTiffanyValentina.Controllers
+{
+    public class Query2and3Controller : ApiController
+    {
+        CovidDeathsDBEntities myDB = new CovidDeathsDBEntities();
+        //// GET api/<controller> for Query 2
+        [Route("api/final/query2")]
+        [HttpGet]
+        public IHttpActionResult GetHamdenJune11()
+        {
+            DateTime JuneDate = new DateTime(2020, 06, 11);
+            var HamdenDeaths = (from NursingHomeDeaths in myDB.NursingHomeDeathsTables
+                                join TownHamden in myDB.TownCTDeathsTables
+                                on NursingHomeDeaths.TownName equals TownHamden.TownName
+                                where TownHamden.Date == JuneDate && NursingHomeDeaths.Date == JuneDate && NursingHomeDeaths.TownName == "Hamden"
+                                select new
+                                {
+                                    NursingHomeDeaths.Date,
+                                    NursingHomeDeaths.NursingHome,
+                                    NursingHomeDeaths.TownName,
+                                    NursingHomeDeaths.COVIDDeath,
+                                    TownHamden.ConfirmedCases
+                                }).Distinct();
+            return Json(HamdenDeaths);
+        }
+
+        // GET api/<controller> for Query 3
+        [Route("api/final/query3")]
+        [HttpGet]
+        public IHttpActionResult GetNursingHomes()
+        {
+            DateTime startDate = new DateTime(2020, 05, 01);
+            DateTime endDate = new DateTime(2020, 05, 31);
+            var NursingHomesMayCovidPositive = (from NursingHomeDeaths in myDB.NursingHomeDeathsTables
+                                                where NursingHomeDeaths.Date > startDate
+                                                where NursingHomeDeaths.Date < endDate
+                                                where NursingHomeDeaths.COVIDPositive > 50
+                                               orderby NursingHomeDeaths.TownName
+                                               select new
+                                               {
+                                                   NursingHomeDeaths.Date,
+                                                   NursingHomeDeaths.TownName,
+                                                   NursingHomeDeaths.NursingHome,
+                                                   NursingHomeDeaths.COVIDPositive
+                                               }).Distinct();
+            return Json(NursingHomesMayCovidPositive);
+        }
+    }
+}
---
+++ ISIT420COVIDFinalTiffanyValentinaSolution/ISIT420COVIDFinalTiffanyValentina/Scripts/App2.js
@@ -0,0 +1,71 @@
+﻿document.addEventListener("DOMContentLoaded", function (event) {
+
+    document.getElementById("nursinghomes").addEventListener("click", function () {
+        GetShowDataQuery2();
+        GetShowDataQuery3();
+    });
+
+});
+function GetShowDataQuery2() {
+    $.getJSON("api/final/query2")
+        .done(function (data) {
+            let table = document.getElementById("table2");
+            console.log(data);
+            generateTable(table, data);
+        });
+}
+function GetShowDataQuery3() {
+    $.getJSON("api/final/query3")
+        .done(function (data) {
+            let table = document.getElementById("table3");
+            console.log(data);
+            generateTable(table, data);
+        });
+}
+function generateTable(table, data) {
+    for (let element of data) {
+        let row = table.insertRow();
+        for (key in element) {
+            let cell = row.insertCell();
+            let text = document.createTextNode(element[key]);
+            cell.appendChild(text);
+        }
+    }
+}
+//function GetShowDataQuery3() {
+//    $.getJSON("api/final/query3")
+//        .done(function (data) {
+//            let table = document.getElementById("table3");
+//            console.log(data);
+//            $.each(data, function (key, item) {
+//                console.log(data);
+//                for (let element of data) {
+//                    let row = table.insertRow();
+//                    for (key in element) {
+//                        let cell = row.insertCell();
+//                        let text = document.createTextNode(item);
+
+//                        cell.appendChild(text);
+//                    }
+//                }
+//            });
+//        });
+//}
+//function GetShowDataQuery3() {
+//    $.getJSON("api/final/query3")
+//        .done(function (data) {
+//            //document.getElementById("displayRace2").innerHTML = data[0] + "   " + data[1] + "   " + data[2] + "   " + data[3];
+//            let table = document.getElementById("table3");
+//            console.log(data);
+//            let row = table.insertRow();
+//            $.each(data, function (key, item) {
+//                console.log(data);
+//                let cell = row.insertCell();
+//                let text = document.createTextNode(item);
+//                cell.appendChild(text);
+//            });
+
+
+//        });
+//}
+
